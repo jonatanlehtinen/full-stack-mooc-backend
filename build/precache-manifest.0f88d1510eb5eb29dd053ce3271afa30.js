147dd2a66be75fc3c2494b33441033add045cba6
Jonatan Lehtinen <=>
2020-04-13 23:35:45 +0300

    Add frontend build

---
+++ build/precache-manifest.0f88d1510eb5eb29dd053ce3271afa30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36ee96ae4dcb539e960ba78d512af433",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8e8c62a4fa295a107d2",
+    "url": "/static/css/main.19b25ac4.chunk.css"
+  },
+  {
+    "revision": "c816bcf7231dd4c169fa",
+    "url": "/static/js/2.e7311442.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e7311442.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8e8c62a4fa295a107d2",
+    "url": "/static/js/main.0dc26fb7.chunk.js"
+  },
+  {
+    "revision": "c940798f285500efd361",
+    "url": "/static/js/runtime-main.64749484.js"
+  }
+]);
